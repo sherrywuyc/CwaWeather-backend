786e0ce95ddd8f7f9ea4758b429af5daea5f319f
sherryyc <sherry.wu.yc@gmail.com>
2025-12-06 23:44:59 +0800

    fix: 更新API請求的地點為基隆市

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "宜蘭縣",
+          locationName: "基隆市",
         },
       }
     );
@@ -48,7 +48,7 @@ const getKaohsiungWeather = async (req, res) => {
     if (!locationData) {
       return res.status(404).json({
         error: "查無資料",
-        message: "無法取得高雄市天氣資料",
+        message: "無法取得天氣資料",
       });
     }
 
